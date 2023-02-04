7f35b1f304c69ca25436fc63a1d1dbca90ecea9c
Lexa <117041725+LexaUZh@users.noreply.github.com>
2023-02-04 15:14:30 +0300

    go-go1

--- coachApp.py
+++ coachApp.py
@@ -1,9 +1,4 @@
 #-*- coding: cp1251 -*-
-from kivy.config import Config
-Config.set('graphics', 'resizable', 0)
-Config.set('graphics', 'width', 350)
-Config.set('graphics', 'height', 450)
-
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.button import Button
